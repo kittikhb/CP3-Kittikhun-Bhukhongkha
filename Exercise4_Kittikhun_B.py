aba0604931bd5fc955fb4d13035365079b20f800
kittikhb <65772457+kittikhb@users.noreply.github.com>
2020-05-31 14:08:53 +0700

    Add files via upload

---
+++ Exercise4_Kittikhun_B.py
@@ -0,0 +1,13 @@
+print("---Your Scores---")
+'''
+โปรแกรมแสดงผลคะแนนปี 2 เทอม 1
+'''
+FE = 90.5
+GenBus = 80
+IntroCompSys = 85
+CompProg = 70.5
+
+print("Foundation English :",FE)
+print("General Bussiness :",GenBus)
+print("Intro to Computer System :",IntroCompSys)
+print("Computer Programming :",CompProg)
